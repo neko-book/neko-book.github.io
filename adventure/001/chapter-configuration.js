8627a28b250ce26d226eab02e50e7add3b10be3f
鱼喵 <2825924703@qq.com>
2026-04-06 01:55:54 +0800

    Update chapter-configuration.js

--- adventure/001/chapter-configuration.js
+++ adventure/001/chapter-configuration.js
@@ -1,4 +1,6 @@
 export default [
-{ title: '第一章 启程', file: 'ch1.txt' },
-{ title: '第二章 迷雾', file: 'ch2.txt' }
+{ title: '序章 云鲸城之初', file: '00.txt' },
+{ title: '第一章 星辰低语', file: '01.txt' },
+{ title: '第二章 静语回响', file: '02.txt' },
+{ title: '第三章 泉心低语', file: '03.txt' },
 ];
