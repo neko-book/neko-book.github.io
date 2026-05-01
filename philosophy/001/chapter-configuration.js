e35d7dcdfceded21d34ee10ae59583d2f7efdcd9
鱼喵 <2825924703@qq.com>
2026-05-01 23:35:10 +0800

    Update chapter-configuration.js

--- philosophy/001/chapter-configuration.js
+++ philosophy/001/chapter-configuration.js
@@ -1,6 +1,3 @@
 export default [
-{ title: '序章 云鲸城之初', file: '00.txt' },
-{ title: '第一章 星辰低语', file: '01.txt' },
-{ title: '第二章 静语回响', file: '02.txt' },
-{ title: '第三章 泉心低语', file: '03.txt' },
+{ title: '第一章 损耗品', file: '00.txt' }
 ];
